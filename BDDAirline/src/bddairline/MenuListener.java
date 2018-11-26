bc4bf3cdf547258a7dda15fb35557a50c7497ac4
CedricHoa <43963265+CedricHoa@users.noreply.github.com>
2018-11-26 18:25:38 +0100

    Delete MenuListener.java

--- BDDAirline/src/bddairline/MenuListener.java
+++
@@ -1,9 +0,0 @@
-package bddairline;
-import java.awt.event.*;
-        
-public class MenuListener implements ActionListener{
-    public void actionPerformed(ActionEvent e){
-        String commande=e.getActionCommand();
-        BDDAirline.faireRequete("SELECT * FROM "+commande);
-    }
-}
